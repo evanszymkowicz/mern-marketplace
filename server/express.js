e01b5267968dac5998d616b47feb87bef96d1805
Evan Szymkowicz <evan.szymkowicz@evanwolf.co>
2019-09-19 09:48:25 -0400

    client side

--- server/express.js
+++ server/express.js
@@ -1,9 +1,31 @@
+import React from "react";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
+
+// middle modules
+import helmet from "helmet";
+import Template from "./../template";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
+import shopRoutes from "./routes/shop.routes";
+import productRoutes from "./routes/product.routes";
+import orderRoutes from "./routes/order.routes";
+
+// server-side modules
+import ReactDOMServer from "react-dom/server";
+import MainRouter from "./../client/MainRouter";
+import StaticRouter from "react-router-dom/StaticRouter";
+
+// Front end modules
+
+import { SheetsRegistry } from "react-jss/lib/jss";
+import JssProvider from "react-jss/lib/JssProvider";
+// import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from "material-ui/styles";
+// import { blueGrey, lightGreen } from "material-ui/colors";
 
 // Catch unauthorised errors
 app.use((err, req, res, next) => {
